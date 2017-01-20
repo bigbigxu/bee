0886cec6965a4106de0a8017ddb09ee91341697b
VigoXu <VigoXu@boyaa.com>
2017-01-20 15:13:26 +0800

    增加php register_shutdown_function

--- server/BaseServer.php
+++ server/BaseServer.php
@@ -177,6 +177,7 @@ class BaseServer
         foreach ($env as $key => $value) {
             ini_set($key, $value);
         }
+        register_shutdown_function(array($this, 'shutdownFunction'));
     }
 
     /**
@@ -1226,7 +1227,7 @@ class BaseServer
      */
     public function saveTaskData()
     {
-        $file = $this->c('server.run_dir') . '/data_' . $this->getWorkerId();
+        $file = $this->c('server.log_dir') . '/data_' . $this->getWorkerId();
         file_put_contents($file, serialize($this->data));
     }
 
@@ -1235,9 +1236,16 @@ class BaseServer
      */
     public function loadTaskData()
     {
-        $file = $this->c('server.run_dir') . '/data_' . $this->getWorkerId();
-        $str = file_get_contents($file);
-        $this->data = unserialize($str);
-        @unlink($file);
+        $file = $this->c('server.log_dir') . '/data_' . $this->getWorkerId();
+        if (is_file($file)) {
+            $str = file_get_contents($file);
+            $this->data = unserialize($str);
+            @unlink($file);
+        }
+    }
+
+    public function shutdownFunction()
+    {
+        $this->saveTaskData();
     }
 }
